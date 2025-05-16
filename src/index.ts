9ff6407c56e5b903522ca97303c11166e993540c
nelsonmidis <nelsonmidi@gmail.com>
2025-05-16 10:25:44 -0300

    curso de api - coloquei id

--- src/index.ts
+++ src/index.ts
@@ -8,29 +8,29 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Bem Vindo ao Curso de Node Js - ver5 - usando npm start");
 });
 
+// id 
+let id = 0;
+
 // lista global 
 
-let usuarios = [{
-        nome: "Darth Vader",
-        idade: 40
-    }, {
-        nome: "Amarildinha",
-        idade: 35
-    }, {
-        nome: "Arthur Sahur",
-        idade: 6
-    }];
+let usuarios: {id: number, nome: string, email: string}[] = [];
 
 // app get 
 
 app.get("/users", (req: Request, res: Response) => {    
     res.send(usuarios);
 });
+ 
+// app get para pegar só um registro
+// dependendo da info que eu enviar 
+
+
 
 // app post 
 
 app.post("/users", (req: Request, res: Response) => {
 let user = req.body;
+user.id = ++id;
 usuarios.push(user);
 res.send({
     message: "Usuario criado com sucesso!"
