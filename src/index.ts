bd0940e2f3ee749779ad0d0b450e88a3b2dca139
nelsonmidis <nelsonmidi@gmail.com>
2025-05-16 20:09:48 -0300

    curso de api - controllers - erros aparecem como resultado

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,10 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import { routes } from "./routes/index";
 // app nao é uma palavra nativa. Neste caso ea está sendo um "tipo" de express
 const app = express();
 
-app.use(express.json());
-
-app.get("/", (req: Request, res: Response) => {
-    res.send("Bem Vindo ao Curso de Node Js - ver5 - usando npm start");
-});
-
-// id 
-let id = 0;
-type User = {id: number; nome: string; email: string;};
-
-// lista global 
-
-let usuarios: User[] = [];
-
-// app get 
-
-app.get("/users", (req: Request, res: Response) => {    
-    res.send(usuarios);
-});
- 
-// app get para pegar só um registro
-// dependendo da info que eu enviar 
-
-app.get("/users/:id", (req: Request, res: Response) => {
-let userId = Number(req.params.id);
-let user = usuarios.find(user => user.id === userId);
-res.send(user);
-});
-
-// app post 
-
-app.post("/users", (req: Request, res: Response) => {
-let user = req.body;
-user.id = ++id;
-usuarios.push(user);
-res.send({
-    message: "Usuario criado com sucesso!"
-});
-});
-
-// app.put
-
-app.put("/users/:id", (req: Request, res: Response) => {
-    let userId = Number(req.params.id);
-    let user = req.body; 
-    let indexOf = usuarios.findIndex(_user => _user.id === userId);
-    usuarios[indexOf].nome = user.nome;
-    usuarios[indexOf].email = user.email;
-    res.send({
-        message: "Usuario alterado com sucesso!"
-    })
-
-});
-
-// app delete
-app.delete("/users/:id", (req: Request, res: Response) => {
-    let userId = Number(req.params.id);
-    let indexOf = usuarios.findIndex((user: User) => user.id === userId);
-    usuarios.splice(indexOf, 1);
-    res.send({
-        message: "Usuario removido com sucesso!"
-    });
-});
+// nao sei o que significa
+routes(app);
 
 // app listen 
 
