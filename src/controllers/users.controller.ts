23c20d37160dc71e01124503551ff6d6a2df83da
nelsonmidis <nelsonmidi@gmail.com>
2025-05-18 19:52:46 -0300

    fazendo experiencias com erros - TENTANDO VARIOS try / catch NAS OUTRAS FUNCOES - aula 4 23 - 18 05 2025

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -14,7 +14,7 @@ type User = {
 
 export class UsersController {
     // função getAll
-    // COLOCANDO UM ERRO DE PROPOSITO 
+    // COLOCANDO UM ERRO DE PROPOSITO - usando try catch
     static async getAll(req: Request, res: Response) {
         try {
             const snapshot = await getFirestore().collection("users").get();
@@ -33,25 +33,44 @@ export class UsersController {
         }
     }
     // função getById
+    // TB COLOCANDO TRY CATCH
     static async getById(req: Request, res: Response) {
-let userId = req.params.id;
+try {
+    let userId = req.params.id;
 const doc = await getFirestore().collection("users").doc(userId).get();
 let user = {
     id: doc.id,
     ...doc.data()
 }
-
+    // throw new Error("Erro ao ENVIAR RESPOSTA");
 res.send(user);
+} catch (error) {
+
+            res.status(500).send({
+                message: "Erro interno do Servidor no getById!!"
+            });    
+}
 }
     // função save
     static async save(req: Request, res: Response) {
+        try {
+            
     let user = req.body;
+        // throw new Error("Erro ao GRAVAR REGISTRO");
     const userSalvo =await getFirestore().collection("users").add(user);
 
     res.status(201).send({
         message: `Usuario ${userSalvo.id} criado com sucesso!`
     });
+    
+        } catch (error) {
+                res.status(500).send({
+                message: "Erro interno do Servidor no SAVE!!"
+            });            
+        }
     }
+
+
     // função update
     static update(req: Request, res: Response) {
         let userId = req.params.id;
@@ -68,11 +87,23 @@ res.send(user);
     }
 
     // função delete
+    // ESTOU COLOCANDO TRY CATCH
     static async delete(req: Request, res: Response) {
+        try {
     let userId = req.params.id;
+        // throw new Error("Erro ao DELETAR REGISTRO");
     await getFirestore().collection("users").doc(userId).delete();
     res.status(204).end();
-}
+
+        } catch (error) {
+            
+                res.status(500).send({
+                message: "Erro interno do Servidor no DELETE!!"
+            });            
+        
+            
+        }
+    }
 
 
 }
